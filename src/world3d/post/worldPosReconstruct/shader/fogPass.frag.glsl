85c78338db603330a367172e0ee600156de45229
DougLilliequist <douglas.lilliequist@hyperisland.se>
2021-04-29 12:45:52 +0300

    using straight up depth buffer for fog

--- src/world3d/post/worldPosReconstruct/shader/fogPass.frag.glsl
+++ src/world3d/post/worldPosReconstruct/shader/fogPass.frag.glsl
@@ -12,11 +12,12 @@ uniform float _Time;
 varying vec3 vViewRay;
 varying vec2 vUv;
 
-//https://www.geeks3d.com/20091216/geexlab-how-to-visualize-the-depth-buffer-in-glsl/
+// #define USING_WORLDPSPACE_POSITION
+
 float LinearizeDepth(float depth) 
 {
-    float z = depth;
-    return (2.0 * _Near) / (_Far + _Near - z * (_Far - _Near));
+    float z = depth * 2.0 - 1.0; // back to NDC 
+    return (2.0 * _Near * _Far) / (_Far + _Near - z * (_Far - _Near));	
 }
 
 vec3 hash32(vec2 p)
@@ -30,22 +31,31 @@ void main() {
 
     float depth = LinearizeDepth(texture2D(_Depth, vUv).x);
 
-    vec3 viewRay = vViewRay * depth;
-    vec3 worldPos = viewRay + _CameraWorldPos;
-
     vec3 col = texture2D(_BasePass, vUv).xyz;
 
+    #ifdef USING_WORLDPSPACE_POSITION
+
+    vec3 viewRay = vViewRay * (depth/_Far);
+
+    //Have to subtract with the camera position as OGL
+    //looks down the positive Z axis
+    vec3 worldPos = viewRay - _CameraWorldPos;
     float dist = length(worldPos);
-    float fog = smoothstep(_Near, _Far, dist);
-    // col = mix(col, vec3(0.8, 0.84, 0.93), fog);
+    float fog = smoothstep(0.0, 20.0, dist);
+
+    #else
+
+    float fog = smoothstep(_Near, _Far*0.31, depth);
+
+    #endif
+
     col = mix(col, vec3(0.7, 0.8, 0.93), fog);
-    // col = mix(col, vec3(0.7, 0.8, 0.93), smoothstep(0.0, 0.3, depth));
-    // col = mix(col, vec3(0.8, 0.84, 0.93), fog);
 
     vec3 hash1 = hash32(gl_FragCoord.xy+fract(_Time)*1300.0);
     vec3 hash2 = hash32(gl_FragCoord.yx+fract(_Time+0.3123)*1300.0);
     vec3 dither = ((hash1) + (hash2-1.0)) / 255.0;
 
     gl_FragColor = vec4((col*0.85)+ dither, 1.0);
+    // gl_FragColor = vec4(vec3(worldPos), 1.0);
 
 }
--- src/world3d/post/worldPosReconstruct/shader/fogPass.vert.glsl
+++ src/world3d/post/worldPosReconstruct/shader/fogPass.vert.glsl
@@ -13,10 +13,17 @@ uniform vec3 _FrustumCorners[4];
 varying vec3 vViewRay;
 varying vec2 vUv;
 
+// #define USING_WORLDPSPACE_POSITION
+
 void main() {
 
     gl_Position = vec4(position, 1.0);
+
+    #ifdef USING_WORLDPSPACE_POSITION
     vViewRay = (_MainCameraInvViewMatrix * vec4(_FrustumCorners[int(frustumCornerIndex)], 0.0)).xyz;
+    #else
+    vViewRay = vec3(0.0);
+    #endif
     vUv = uv;
 }
 
